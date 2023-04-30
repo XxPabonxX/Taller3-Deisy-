cd22c297ff7f5d69224d3785fc4535c944860db3
XxPabonxX <diego.pabon@udea.edu.co>
2023-04-29 21:41:16 -0500

    Se agrega el tercer punto completo

---
+++ Punto3.py
@@ -0,0 +1,31 @@
+#3.	Elaborar un programa en Python que permita crear una lista con los números enteros 
+#   entre 1 y 20  (x) y de forma paralela otra lista con enteros aleatorios entre 1 y 6.
+
+!pip install matplotlib
+
+# Importar las librerías necesarias
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Crear la lista x con los números enteros entre 1 y 20
+x = list(range(1, 21))
+
+# Crear la lista y con enteros aleatorios entre 1 y 6
+y = [random.randint(1, 6) for i in range(20)]
+
+#3.1 Mostrar las dos listas
+print("Lista x:", x)
+print("Lista y:", y)
+
+#3.2 Crear el gráfico de barras
+plt.bar(x, y)
+plt.title("Gráfico de barras")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
+
+#3.3 Mostrar las medidas estadísticas de la lista y
+print("Media:", np.mean(y))
+print("Mediana:", np.median(y))
+print("Desviación estándar:", np.std(y))
